0bbeb0c9c3b94b9442b61f2fe9207ef9159df230
TanyaArdhia <tanyaardhiap@gmail.com>
2024-07-11 17:16:46 +0700

    first commit

--- movie-db/src/views/home.jsx
+++ movie-db/src/views/home.jsx
@@ -1,6 +1,46 @@
-export function Home(){
-    return(
-        <>
-        <h1>helooo</h1></>
-    )
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Barcode from "react-barcode";
+
+export function Home() {
+  const [getData, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const api = await axios.get(
+        "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_API_MOVIE}`,
+          },
+        }
+      );
+
+      console.log(api.data, ">>> result");
+      setData(api.data.results);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Movie List</h1>
+      {error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <ul>
+          {getData.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
